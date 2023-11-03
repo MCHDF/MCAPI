aeb26d26a9ac9a63fa4a4c4f9837b00c1d8b3122
MCHDF <j506661@gmail.com>
2023-11-03 16:12:35 +0900

    rename group

--- src/main/java/moe/mchdf/mcapi/McapiApplication.java
+++ src/main/java/moe/mchdf/mcapi/McapiApplication.java
@@ -1,4 +1,4 @@
-package com.example.mcapi;
+package moe.mchdf.mcapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/moe/mchdf/mcapi/McapiApplicationTests.java
+++ src/test/java/moe/mchdf/mcapi/McapiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.mcapi;
+package moe.mchdf.mcapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
