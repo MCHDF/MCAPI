00c358b589c9b0845be245f96056c1b548e9d204
MCHDF <j506661@gmail.com>
2023-11-03 16:05:44 +0900

    MCAPI 프로젝트 시잗

---
+++ src/main/java/com/example/mcapi/McapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mcapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McapiApplication.class, args);
+    }
+
+}
